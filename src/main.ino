eb3afa79c38980e18bc0f934b776ebac70ad0ad3
Joshua Zenn <wordman05@gmail.com>
2015-06-26 19:57:23 -0400

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,25 @@
+int CIBUILDNUMBER = 0;
+
 void setup()
 {
-  
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  Serial.begin(9600);
+  Serial.println("Octoduino build: " + CIBIULDNUMBER);
+  Serial.println("Copyright 2015 - NETponents");
+  Serial.println("Distributed under the GNU GPL v2.0 license");
+  Serial.println("Commercial use with this build of Octoduino is prohibited");
+  digitalWrite(13, LOW);
+  Serial.println("");
+  digitalWrite(13, HIGH);
+  Serial.println("Starting up...");
+  digitalWrite(13, LOW);
 }
 void loop()
 {
-  
+  // Before script
+  digitalWrite(13, HIGH);
+  // Do stuff
+  digitalWrite(13, LOW);
+  // After script
 }
