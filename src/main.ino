c584853d2b1812e09cf950e80446e84c2cd7e78c
Joshua Zenn <wordman05@gmail.com>
2015-06-29 15:51:27 -0400

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -3,13 +3,14 @@
 
 bool ShowDebug = true;
 bool fatalCrash = false;
+int language = 0;
 
 void setup()
 {
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
   Serial.begin(9600);
-  sPrintLn("Octoduino build: Error");
+  sPrintLn("Octoduino build: ERR 0x0");
   sPrintLn("Copyright 2015 - NETponents");
   sPrintLn("Distributed under the GNU GPL v2.0 license");
   sPrintLn("Commercial use with this build of Octoduino is prohibited");
@@ -17,7 +18,7 @@ void setup()
   pinMode(10, OUTPUT);
   if (!SD.begin(4))
   {
-    sPrintLn("ERR 0x01");
+    sPrintLn("ERR 0x1");
     fatalCrash = true;
     return;
   }
@@ -25,10 +26,13 @@ void setup()
   if (configFile)
   {
     sPrintLn("Configuration file found");
-    while (configFile.available())
-    {
-      Serial.println(configFile.read());
-    }
+    //while (configFile.available())
+    //{
+    //  while (configFile.peek() != ";")
+    //  {
+    //    configFile.read();
+    //  }
+    //}
       configFile.close();
   }
   else
@@ -38,6 +42,10 @@ void setup()
     fatalCrash = true;
     return;
   }
+  //Begin parsing the PARSEBASIC main script
+  Serial.println("Launching bootloader");
+  File bootloader = SD.open("bootloader.pba");
+  
 }
 void loop()
 {
@@ -50,6 +58,35 @@ void loop()
     // Before script
     // Do stuff
     // After script
+  }
+}
+void parseConfigLine(char line[])
+{
+  switch (line[0])
+  {
+    case '0':
+      //Language
+      Language = (int)line[1];
+      break;
+    case '1':
+      //Debug
+      if(line[1] == '0')
+      {
+        ShowDebug = false;
+      }
+      else
+      {
+        ShowDebug = true;
+      }
+      break;
+    case '2':
+      //Do something
+      break;
+    case '3':
+      //Do something
+      break;
+    default:
+      Serial.println("ERR: 0x2")
   }
 }
 void sPrintLn(const char message[])
