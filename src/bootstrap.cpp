58263e809aa21e49723a8878502b0b32bca62d2e
Joshua Zenn <wordman05@gmail.com>
2015-06-30 16:53:01 -0400

    Update bootstrap.cpp

--- src/bootstrap.cpp
+++ src/bootstrap.cpp
@@ -1,8 +1,34 @@
 #include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
 
 void initBootstrap()
 {
-  //TODO: Initialize SD module
-  //TODO: Open bootloader.pba
-  //TODO: Begin parsing BIOS PBA code
+  pinMode(10, OUTPUT);
+  SD.begin(4);
+  //Begin parsing the PARSEBASIC main script
+  Serial.println("Launching bootloader");
+  File bootloader = SD.open("bootloader.pba");
+  if(bootloader)
+  {
+    sPrintLn("Booting...");
+    char terminator = ';';
+    while (bootloader.available())
+    {
+      String cmd = "";
+      while (char(bootloader.peek()) != terminator)
+      {
+        cmd += bootloader.read();
+      }
+      bootloader.read();
+      parseBasic(cmd);
+    }
+    bootloader.close();
+  }
+  else
+  {
+    sPrintLn("ERR: 0x4");
+    fatalCrash = true;
+    return;
+  }
 }
