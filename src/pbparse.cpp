5f1575fb5086caaa1ac30b4d98164a208553c2cf
Joshua Zenn <wordman05@gmail.com>
2015-06-30 16:58:57 -0400

    Create pbparse.cpp

---
+++ src/pbparse.cpp
@@ -0,0 +1,16 @@
+void PBstop()
+{
+  Serial.println("Program has finished. Terminating.");
+  while(true)
+  {
+    
+  }
+}
+void PBcrash()
+{
+  Serial.println("ParseBasic parser has encountered an error. Terminating.");
+  while(true)
+  {
+    
+  }
+}
