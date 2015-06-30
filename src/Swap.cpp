8e7d86364c931c87fb1d7fdcc68b2ace7762e06f
Joshua Zenn <wordman05@gmail.com>
2015-06-30 16:38:51 -0400

    Create Swap.cpp

---
+++ src/Swap.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+void Swap::init()
+{
+  //Initialize the swapfile
+}
+void Swap::create(String name, String value)
+{
+  //Create an entry
+}
+String Swap::read(String name)
+{
+  //Read an entry
+}
+void Swap::update(String name, String value)
+{
+  this.delete(name);
+  this.create(name, value);
+}
+void Swap::delete(String name)
+{
+  //Delete an entry
+}
