429524ad7babe9187216d5edf47614b83f345cc3
Joshua Zenn <jzenn@students.kennesaw.edu>
2015-07-01 00:57:40 +0000

    Fixed char* length

--- src/swap.cpp
+++ src/swap.cpp
@@ -17,7 +17,7 @@ void swapinit()
 }
 char* swapGetPath(String name)
 {
-  char result[] = "/swap/" + name.c_str + ".swp"
+  char result[10 + name.length()] = "/swap/" + name.c_str + ".swp"
   return result;
 }
 void swapcreate(String name, String value)
