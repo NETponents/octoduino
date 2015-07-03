0decc0c0ed34fc1ce5553a9c292487a146b525db
Joshua Zenn <wordman05@gmail.com>
2015-07-03 15:05:15 -0400

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -7,6 +7,7 @@ void setup()
 {
   pinMode(13, OUTPUT);
   Serial.begin(9600);
+  outinit();
   outwrite("Octoduino build: ERR 0x1");
   outwrite("Copyright 2015 - NETponents");
   outwrite("Distributed under the GNU GPL v2.0 license");
