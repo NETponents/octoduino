c29e4e8dc08731f61032316d3d19f85c09e8b15e
Joshua Zenn <jzenn@students.kennesaw.edu>
2015-07-25 17:56:23 +0000

    Fixed build errors

--- src/pbparse.cpp
+++ src/pbparse.cpp
@@ -39,55 +39,59 @@ void PBparse(String line)
   else
   {
     String opcode = TKgetToken(line, 0);
-    switch(opcode)
+    if(opcode == "PRINT")
     {
-      case "PRINT":
-        outwrite(TKgetToken(line, 1));
-        break;
-      case "NEWPRINT":
-        outwrite("\n");
-        break;
-      case "CREATESWAP":
-        swapinit();
-        break;
-      case "IO":
-        int portn = int(TKgetToken(line, 1).c_str());
-        int state = int(TKgetToken(line, 2).c_str());
-        if(state == '0')
-        {
-          digitalWrite(portn, LOW);
-        }
-        else
-        {
-          digitalWrite(portn, HIGH);
-        }
-        break;
-      case "NEW":
-        String value = TKgetToken(line, 2);
-        if(value == "TKERROR")
-        {
-          value = "";
-        }
-        swapcreate(TKgetToken(line, 1), value);
-        break;
-      case "DELETE":
-        swapdelete(TKgetToken(line, 1));
-        break;
-      case "SET":
-        swapupdate(TKgetToken(line, 1), TKgetToken(line, 2));
-        break;
-      case "EXTLOAD":
-        PBstart(TKgetToken(line, 1).c_str());
-        break;
-      case "END":
-        PBstop();
-        break;
-      case "TKERROR":
-        PBcrash();
-        break;
-      case default:
-        PBcrash();
-        break;
+      outwrite(TKgetToken(line, 1));
+    }
+    else if(opcode == "NEWPRINT")
+    {
+      outwrite("\n");
+    }
+    else if(opcode == "CREATESWAP")
+    {
+      swapinit();
+    }
+    else if(opcode == "IO")
+    {
+      int portn = int(TKgetToken(line, 1).c_str());
+      int state = int(TKgetToken(line, 2).c_str());
+      if(state == '0')
+      {
+        digitalWrite(portn, LOW);
+      }
+      else
+      {
+        digitalWrite(portn, HIGH);
+      }
+    }
+    else if(opcode == "NEW")
+    {
+      String value = TKgetToken(line, 2);
+      if(value == "TKERROR")
+      {
+        value = "";
+      }
+      swapcreate(TKgetToken(line, 1), value);
+    }
+    else if(opcode == "DELETE")
+    {
+      swapdelete(TKgetToken(line, 1));
+    }
+    else if(opcode == "SET")
+    {
+      swapupdate(TKgetToken(line, 1), TKgetToken(line, 2));
+    }
+    else if(opcode == "EXTLOAD")
+    {
+      PBstart(TKgetToken(line, 1).c_str());
+    }
+    else if(opcode == "END")
+    {
+      PBstop();
+    }
+    else
+    {
+      PBcrash();
     }
   }
 }
