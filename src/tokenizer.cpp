c6e2b47f5a7dcc0def8ac3ae1ae801a14ad33fb0
Joshua Zenn <wordman05@gmail.com>
2015-08-04 11:56:40 -0400

    Commenting

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -2,29 +2,39 @@
 
 String TKgetToken(String line, int tokenIndex)
 {
+    // Create buffer for output
     String resultToken = "";
+    // Initialize index counters
     int counter = -1;
     int tcounter = -1;
+    // Trim whitespace on input
     line.trim();
+    // Set maximums
     int length = line.length();
+    // State variables
     boolean isend = false;
     bool isstring = false;
+    // Token loop
     while(true)
     {
         tcounter++;
+        // Char loop
         while(true)
         {
             counter++;
+            // Check char pointer against maximum
             if(counter >= length - 1)
             {
                 isend = true;
                 break;
             }
+            // Check for instruction terminator
             else if(line.charAt(counter == ';'))
             {
                 isend = true;
                 break;
             }
+            // Check for start or end of string literal
             else if(line.charAt(counter == '"'))
             {
                 if(isstring)
@@ -36,6 +46,7 @@ String TKgetToken(String line, int tokenIndex)
                     isstring = true;
                 }
             }
+            // Check for token separator
             else if(line.charAt(counter) == ' ')
             {
                 if(isstring)
@@ -47,25 +58,30 @@ String TKgetToken(String line, int tokenIndex)
                     break;
                 }
             }
+            // No special instructions, record char at pointer
             else
             {
                 resultToken = resultToken + line.charAt(counter);
             }
         }
+        // Check current token against desired token index
         if(tcounter == tokenIndex)
         {
             return resultToken;
         }
+        // Check if end of string
         else if(isend == true)
         {
             resultToken = "TKERROR";
             return resultToken;
         }
+        // Clear token buffer and restart loop on next token
         else
         {
             resultToken = "";
         }
     }
+    // Token index not found, send error
     resultToken = "TKERROR";
     return resultToken;
 }
