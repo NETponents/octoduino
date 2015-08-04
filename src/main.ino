4f5e91c33d6f2c488869ec003e9425f0a6e19a32
Joshua Zenn <wordman05@gmail.com>
2015-08-04 11:42:15 -0400

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,21 +1,25 @@
 #include <SD.h>
-#include <SPI.h>
+#include <SPI.h> // Required for PlatformIO
 #include "bootstrap.h"
 #include "output.h"
 
 void setup()
 {
+  // Status LED
   pinMode(13, OUTPUT);
+  // Open serial
   Serial.begin(9600);
   while(!Serial)
   {
-    ;
+    ; // Leonardo: wait for serial to connect
   }
+  // Initialize output class
   outinit();
   outwrite("Octoduino v0.1.0");
   outwrite("Copyright 2015 - NETponents");
   outwrite("Distributed under the GNU GPL v2.0 license");
   outwrite("Commercial use with this build of Octoduino is prohibited");
+  // Start the bootloader
   initBootstrap();
 }
 void loop()
