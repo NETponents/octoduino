af5cecc79127c6bb7142725c8a464ce68ca0d6e4
Joshua Zenn <wordman05@gmail.com>
2015-08-04 11:43:56 -0400

    Commenting

--- src/output.cpp
+++ src/output.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
 #include <SD.h>
-#include <SPI.h>
+#include <SPI.h> // Required for PlatformIO
 #include "output.h"
 
 void outinit()
 {
+  // Initialize text logs on SD card
   File logger = SD.open("/log.txt");
   logger.println("Logging started");
 }
 void outwrite(String msg)
 {
+  // Print message to serial
   Serial.println(msg);
+  // Also print message to logger
   File logger = SD.open("/log.txt");
   logger.println(msg);
   logger.close();
