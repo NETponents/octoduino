7678cc1c4a5d0fb431fa94e342aa48a472dd07df
Joshua Zenn <wordman05@gmail.com>
2015-08-05 13:48:38 -0400

    Header comment

--- src/bootstrap.h
+++ src/bootstrap.h
@@ -1,3 +1,10 @@
+/////////////////////////////////////////////////
+// This software is provided "as is" with      //
+// no warranty of any kind. Use this software  //
+// at your own risk. Distributed under the     //
+// MIT license. Copyright 2015 Joshua Zenn.    //
+/////////////////////////////////////////////////
+
 #ifndef HEADER_BOOTSTRAP
   #define HEADER_BOOTSTRAP
    
