edc21f95d7874aef42c8537c8468e91cb530d786
Joshua Zenn <wordman05@gmail.com>
2015-08-05 13:49:40 -0400

    Header comment

--- src/output.h
+++ src/output.h
@@ -1,3 +1,10 @@
+/////////////////////////////////////////////////
+// This software is provided "as is" with      //
+// no warranty of any kind. Use this software  //
+// at your own risk. Distributed under the     //
+// MIT license. Copyright 2015 Joshua Zenn.    //
+/////////////////////////////////////////////////
+
 #ifndef HEADER_OUTPUT
   #define HEADER_OUTPUT
    
