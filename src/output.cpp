8ea83e079967c908f62a2cd2b3e761df50e54c2a
Joshua Zenn <wordman05@gmail.com>
2015-08-07 09:14:36 -0400

    Update output.cpp

--- src/output.cpp
+++ src/output.cpp
@@ -18,15 +18,20 @@ void outinit()
 }
 void outwrite(String msg)
 {
-  // Get time
+  // Get time (HH:MM:SS)
   unsigned long ms = millis();
   unsigned long mi = ms / 1000;
   ms = ms % 1000;
   unsigned long hr = mi / 60;
   mi = mi % 60;
-  String timestamp = "[" + hr + ":" + mi + ":" + ms + "] ";
-  msg = timestamp + msg;
   // Print message to serial
+  Serial.print("[");
+  Serial.print(hr);
+  Serial.print(":");
+  Serial.print(mi);
+  Serial.print(":");
+  Serial.print(ms);
+  Serial.print("] ");
   Serial.println(msg);
   // Also print message to logger
   File logger = SD.open("/log.txt");
