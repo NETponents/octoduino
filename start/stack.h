3780a3fc6446b5a1f25673bbe601f9ff91831328
Joshua Zenn <wordman05@gmail.com>
2015-08-11 12:41:42 -0400

    Create stack.h

---
+++ start/stack.h
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////
+// This software is provided "as is" with      //
+// no warranty of any kind. Use this software  //
+// at your own risk. Distributed under the     //
+// MIT license. Copyright 2015 Joshua Zenn.    //
+/////////////////////////////////////////////////
+
+#ifndef HEADER_STACK
+  #define HEADER_STACK
+   
+  void stackpush(String filepath);
+  void stackpop(String filepath):
+  String stackdump();
+
+#endif
