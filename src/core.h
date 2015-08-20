b6854dd74eb9fc2d80eeaf40f883ecd3b351d727
Joshua Zenn <wordman05@gmail.com>
2015-08-20 09:43:00 -0400

    Update core.h

--- src/core.h
+++ src/core.h
@@ -2,7 +2,7 @@
 // This software is provided "as is" with      //
 // no warranty of any kind. Use this software  //
 // at your own risk. Protected under the GPL   //
-// v2.0 license. Copyright 2015 Joshua Zenn.   //
+// v2.0 license. Copyright 2015 NETponents.    //
 /////////////////////////////////////////////////
 
 #ifndef HEADER_CORE
